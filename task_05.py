03fb9662b755a4d9953d9a2c9531589b3319fefd
akelaPro <ilya_chernov_2022@mail.ru>
2024-08-20 17:19:01 +0300

    5

--- task_05.py
+++ task_05.py
@@ -2,7 +2,7 @@ import datetime
 def date_in_future(integer):
     current_date = datetime.datetime.today()
     if not integer:        
-        return current_date.replace(microsecond=0).strftime("%d-%m-%Y %H:%M:%S")  # Форматируем дату
+        return current_date.replace(microsecond=0).strftime("%d-%m-%Y %H:%M:%S")
     future_date = datetime.datetime.today() + datetime.timedelta(days=integer)
     future_date = future_date.replace(microsecond=0)
     return future_date.strftime("%d-%m-%Y %H:%M:%S")
